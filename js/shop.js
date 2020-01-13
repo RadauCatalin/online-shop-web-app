e6996cb1c138a6e1002c2b745efe20bf3391a5ed
Catalin <catalin.radau@yahoo.com>
2020-01-13 20:51:28 +0200

    Displaying cart content from api

--- js/shop.js
+++ js/shop.js
@@ -4,12 +4,29 @@ window.Shop = {
     getProducts: function () {
         $.ajax({
             url: Shop.API_BASE_URL + "/products"
-            //default ajax method: "GET"
+            // default ajax method: "GET"
         }).done(function (response) {
             console.log(response);
 
             Shop.displayProducts(response.content);
         });
+    },
+
+    addProductToCart: function (productId) {
+        var request = {
+            // todo: take customer id dynamically somehow
+            customerId: 9,
+            productId: productId
+        };
+
+        $.ajax({
+            url: Shop.API_BASE_URL + "/carts",
+            method: "PUT",
+            contentType: "application/json",
+            data: JSON.stringify(request)
+        }).done(function () {
+            window.location.replace("cart.html");
+        })
     },
 
     getProductHtml: function (product) {
@@ -20,8 +37,8 @@ window.Shop = {
                         </div>
                         <h2><a href="">${product.name}</a></h2>
                         <div class="product-carousel-price">
-                            <ins>$${product.price}</ins>
-                        </div>
+                            <ins>$${product.price}</ins> 
+                        </div>  
                         
                         <div class="product-option-shop">
                             <a class="add_to_cart_button" data-quantity="1" data-product_sku="" data-product_id="${product.id}" rel="nofollow" href="/canvas/shop/?add-to-cart=70">Add to cart</a>
@@ -36,7 +53,20 @@ window.Shop = {
         products.forEach(oneProduct => productsHtml += Shop.getProductHtml(oneProduct));
 
         $(".single-product-area .row:first-child").html(productsHtml);
+    },
+
+    bindEvents: function () {
+        $(".single-product-area").delegate(".add_to_cart_button", "click",
+            function (event) {
+                event.preventDefault();
+
+                let productId = $(this).data("product_id");
+
+                Shop.addProductToCart(productId);
+            })
+
     }
 };
 
 Shop.getProducts();
+Shop.bindEvents();
